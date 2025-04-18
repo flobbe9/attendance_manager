4fc08e85aea0f5680a240cf0c5e06dc55e07bb0c
flobbe9 <florin735@live.com>
2025-04-18 15:56:56 +0200

    refactor: helper select take string as selected option

--- components/helpers/HelperSelect.tsx
+++ components/helpers/HelperSelect.tsx
@@ -8,15 +8,17 @@ import React, { useEffect, useState } from "react";
 import { Animated, BackHandler, StyleProp, Text, useAnimatedValue, ViewProps, ViewStyle } from "react-native";
 import HelperButton from "./HelperButton";
 import HelperText from "./HelperText";
-import { log } from "@/utils/logUtils";
+import { log, logError } from "@/utils/logUtils";
 import { useBackHandler } from "@/hooks/useBackHandler";
 import { DynamicStyles } from "@/abstract/DynamicStyles";
 
 
 interface Props extends HelperProps<ViewStyle>, ViewProps {
     options: string[],
-    selectedOptions: Set<string>,
-    setSelectedOptions: (options: Set<string>) => void,
+    /** Needs to be a set if ```multiselect``` is ```true```, else may be both */
+    selectedOptions: Set<string> | string | undefined,
+    /** Needs to have a set as arg if ```multiselect``` is ```true```, else may be both */
+    setSelectedOptions: (options: Set<string> | string | undefined) => void,
     /** Default is 200 */
     optionsContainerHeight?: number,
     /** 
@@ -66,6 +68,7 @@ export default function HelperSelect({
     useEffect(() => {
         setSelectionButtonValue(getSelectionButtonValue());
         setOptionElements(mapOptions());
+
     }, [selectedOptions]);
 
 
@@ -79,6 +82,7 @@ export default function HelperSelect({
         setAreOptionsVisible(false); 
         // only pop the screen if options werer not visible before
         return areOptionsVisible;
+
     }, [areOptionsVisible]);
 
 
@@ -88,30 +92,43 @@ export default function HelperSelect({
      * @param pressedOption 
      */
     function handleOptionPress(pressedOption: string): void {
-        
-        let updatedSelectedOptions = new Set([...selectedOptions]);
-        
-        if (selectedOptions.has(pressedOption)) {
-            if (multiselect)
+
+        if (multiselect) {
+            if (!(selectedOptions instanceof Set)) {
+                logError("'selectedOptions' needs to be a set if 'multiselect' is true");
+                return;
+            }
+            let updatedSelectedOptions = new Set([...selectedOptions]);
+
+            if (updatedSelectedOptions.has(pressedOption))
                 updatedSelectedOptions.delete(pressedOption);
             else
-                updatedSelectedOptions.clear();
-            
-        } else {
-            if (multiselect)
                 updatedSelectedOptions.add(pressedOption);
-            else
-                updatedSelectedOptions = new Set([pressedOption]);
-        }
 
-        setSelectedOptions(updatedSelectedOptions);
+            setSelectedOptions(updatedSelectedOptions);
+        
+        } else {
+            if (!(selectedOptions instanceof Set)) {
+                selectedOptions = pressedOption;
+                setSelectedOptions(pressedOption);
+                
+            } else {
+                let updatedSelectedOptions = new Set([...selectedOptions]);
+
+                if (!selectedOptions.has(pressedOption))
+                    updatedSelectedOptions = new Set([pressedOption]); 
+
+                setSelectedOptions(updatedSelectedOptions);
+            }
+        }
+    
         setAreOptionsVisible(multiselect);
     }
 
 
     function mapOptions(): JSX.Element[] {
 
-        if (!options || !selectedOptions)
+        if (!options)
             return [];
 
         return options
@@ -123,7 +140,7 @@ export default function HelperSelect({
                     key={i}
                 >
                     <HelperText dynamicStyles={HelperSelectStyles.optionButtonText}>{option}</HelperText>
-                    {selectedOptions.has(option) && <FontAwesome name="check" style={HelperSelectStyles.optionButtonText.default} />}
+                    {isOptionSelected(option) && multiselect && <FontAwesome name="check" style={HelperSelectStyles.optionButtonText.default} />}
                 </HelperButton>
             );
     }
@@ -131,12 +148,25 @@ export default function HelperSelect({
 
     function getSelectionButtonValue(): string {
 
-        if (!options || !selectedOptions || !selectedOptions.size)
+        if (!options || !selectedOptions)
             return noSelectionLabel;
 
-        return options
-            .filter(option => selectedOptions.has(option))
-            .reduce((prev, cur) => `${prev}, ${cur}`);
+        if (selectedOptions instanceof Set) {
+            if (!selectedOptions.size)
+                return noSelectionLabel;
+            
+            return options
+                .filter(option => isOptionSelected(option))
+                .reduce((prev, cur) => `${prev}, ${cur}`);
+        }
+    
+        return selectedOptions;
+    }
+
+
+    function isOptionSelected(optionValue: string): boolean {
+
+        return selectedOptions instanceof Set ? selectedOptions.has(optionValue) : selectedOptions === optionValue;
     }
 
 
@@ -148,7 +178,7 @@ export default function HelperSelect({
                 dynamicStyles={HelperSelectStyles.selectionButton}
                 onTouchStart={() => setAreOptionsVisible(!areOptionsVisible)}
             >
-                <Text style={{...HelperSelectStyles.selectionButtonValue.default, opacity: selectedOptions.size ? 1 : 0.5}}>
+                <Text style={{...HelperSelectStyles.selectionButtonValue.default, opacity: selectionButtonValue === noSelectionLabel ? 0.5 : 1}}>
                     {selectionButtonValue}
                 </Text>
                 <FontAwesome name="chevron-down" style={HelperSelectStyles.selectionButtonValue.default} />
