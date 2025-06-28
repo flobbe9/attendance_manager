7e378db1289e8f50abfe38e36975c98f37f61332
flobbe9 <florin735@live.com>
2025-06-28 13:08:41 +0200

    refactor: log levels

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,17 +1,17 @@
-import { LogLevelName } from "@/abstract/LogLevel";
+import { LogLevel } from "@/abstract/LogLevel";
 import { DrizzleConfig } from "drizzle-orm";
 
 export const APP_VERSION = process.env.EXPO_PUBLIC_APP_VERSION;
 export const DATABASE_NAME = process.env.EXPO_PUBLIC_DATABASE_NAME;
-export const LOG_LEVEL = process.env.EXPO_PUBLIC_LOG_LEVEL;
+export const LOG_LEVEL = LogLevel[process.env.EXPO_PUBLIC_LOG_LEVEL];
 
 // Custom log
-export const LOG_LEVEL_COLORS: Record<LogLevelName, string> = {
-    "INFO": "white",
-    "WARN": "rgb(255, 233, 174)",
-    "ERROR": "rgb(255, 230, 230)",
-    "DEBUG": "rgb(230, 230, 230)",
-    "TRACE": "rgb(170, 210, 255)"
+export const LOG_LEVEL_COLORS: Record<LogLevel, string> = {
+    [LogLevel.INFO]: "white",
+    [LogLevel.WARN]: "rgb(255, 233, 174)",
+    [LogLevel.ERROR]: "rgb(255, 230, 230)",
+    [LogLevel.DEBUG]: "rgb(230, 230, 230)",
+    [LogLevel.TRACE]: "rgb(170, 210, 255)"
 }
 
 /** Dont log to console if the 'message' contains one of these strings */
