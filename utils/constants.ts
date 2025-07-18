25c286b86685cda9885533123c3e3f14f914f8e1
flobbe9 <florin735@live.com>
2025-07-18 10:58:41 +0200

    feat: add secure storage helpers and base64 encode methods

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,16 @@
-import { LogLevel } from "@/abstract/LogLevel";
-import { DrizzleConfig } from "drizzle-orm";
-import ExpoConstants from 'expo-constants';
+import {LogLevel} from "@/abstract/LogLevel";
+import {DrizzleConfig} from "drizzle-orm";
+import ExpoConstants from "expo-constants";
 
-export const APP_VERSION = ExpoConstants.expoConfig.version; // read from app.json
+/** Values from app.json file */
+export const appJson = ExpoConstants.expoConfig;
+
+export const APP_VERSION = appJson.version;
 export const DATABASE_NAME = process.env.EXPO_PUBLIC_DATABASE_NAME;
 export const LOG_LEVEL = LogLevel[process.env.EXPO_PUBLIC_LOG_LEVEL];
+
+/** Auth */
+export const AUTH_APPLE_REDIRECT_URL = process.env.EXPO_PUBLIC_AUTH_APPLE_REDIRECT_URL;
 
 // Custom log
 export const LOG_LEVEL_COLORS: Record<LogLevel, string> = {
@@ -12,12 +18,12 @@ export const LOG_LEVEL_COLORS: Record<LogLevel, string> = {
     [LogLevel.WARN]: "rgb(255, 233, 174)",
     [LogLevel.ERROR]: "rgb(255, 230, 230)",
     [LogLevel.DEBUG]: "rgb(230, 230, 230)",
-    [LogLevel.TRACE]: "rgb(170, 210, 255)"
-}
+    [LogLevel.TRACE]: "rgb(170, 210, 255)",
+};
 
 /** Dont log to console if the 'message' contains one of these strings */
 export const CONSOLE_MESSAGES_TO_AVOID: (string | number)[] = [
-    "Require cycles are allowed, but can result in uninitialized values. Consider refactoring to remove the need for a cycle."
+    "Require cycles are allowed, but can result in uninitialized values. Consider refactoring to remove the need for a cycle.",
 ];
 
 /**
@@ -25,9 +31,9 @@ export const CONSOLE_MESSAGES_TO_AVOID: (string | number)[] = [
  */
 export type CheckboxStatus = "checked" | "indeterminate" | "unchecked";
 
-export const DRIZZLE_DB_CONFIG: DrizzleConfig<Record<string, never>>  = {
-    casing: 'snake_case',
-}
+export const DRIZZLE_DB_CONFIG: DrizzleConfig<Record<string, never>> = {
+    casing: "snake_case",
+};
 
 export const NO_SELECTION_LABEL = "No selection";
 
@@ -35,7 +41,13 @@ export const SQL_BLOB_SIZE = 65_535;
 
 /** Settings */
 /** Whether not to show a popup on invalid attendance input. Default shoujld be false */
-export const SETTINGS_DONT_SHOW_ATTENDANCE_INPUT_VALIDATOIN_ERROR_POPUP_KEY = "dont_show_attendance_input_validation_error_popup";
+export const SETTINGS_DONT_SHOW_ATTENDANCE_INPUT_VALIDATOIN_ERROR_POPUP_KEY =
+    "dont_show_attendance_input_validation_error_popup";
 /** Whether not to show a popup on school subject change. Default shoujld be false */
 export const SETTINGS_DONT_CONFIRM_SCHOOL_SUBJECT_CHANGE_KEY = "dont_confirm_school_subject_change";
 export const SETTINGS_DONT_CONFIRM_ATTENDANCE_SCREEN_LEAVE = "dont_confirm_attendance_screen_leave";
+
+/** Other */
+/** Alpha-numeric chars, `_`, `-` and `.` */
+export const SECURE_STORAGE_KEY_REGEX: RegExp = /^[\w\-_\.]*$/;
+export const SECURE_STORAGE_MAX_VALUE_LENGTH: number = 2048;
--- utils/projectUtils.ts
+++ utils/projectUtils.ts
@@ -1,70 +1,108 @@
-import { assertFalsyAndThrow, isAnyFalsy } from "./utils";
+import {logDebug} from "./logUtils";
+import {Buffer} from "buffer";
+import {assertFalsyAndThrow, isAnyFalsy, isBlank} from "./utils";
 
 export function formatDateGermanNoTime(date: Date): string {
-    if (!date)
-        return '-';
+    if (!date) return "-";
 
     return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
 }
 
-
 export function parseNumOrThrow(numString: string): number {
-
     assertFalsyAndThrow(numString);
 
     const num = Number(numString);
 
-    if (isNaN(num))
-        throw new Error(`Failed to parse number '${numString}'`);
+    if (isNaN(num)) throw new Error(`Failed to encode number '${numString}'`);
 
     return num;
 }
-
 
 export function parseNumOrReturnNull(numString: string): number | null {
-
     const num = Number(numString);
 
-    if (isNaN(num) || isAnyFalsy(numString))
-        return null; 
+    if (isNaN(num) || isAnyFalsy(numString)) return null;
 
     return num;
 }
-
 
 /**
  * Use this to compare primitive values.
- * 
- * @param val1 
- * @param val2 
+ *
+ * @param val1
+ * @param val2
  * @param considerDistintFalsyValues if `true` values are considered not equal if they have different falsy values, e.g. `null` and `undefined`. Default is `false`
  * @returns `val1 === val2` and by default considering 2 falsy values equal
  */
 export function defaultEquals<T>(val1: T, val2: T, considerDistintFalsyValues = false): boolean {
+    if (considerDistintFalsyValues) return val1 === val2;
 
-    if (considerDistintFalsyValues)
-        return val1 === val2;
+    if (!val1) return !val2;
 
-    if (!val1)
-        return !val2;
-
-    if (!val2)
-        return false;
+    if (!val2) return false;
 
     return val1 === val2;
 }
 
-
 /**
- * @param val1 
- * @param val2 
+ * @param val1
+ * @param val2
  * @param considerDistintFalsyValues if `true` values are considered not equal if they have different falsy values, e.g. `null` and `undefined`. Default is `false`
  * @returns `val1 === val2` but only if one of the values is falsy, else `null`
  */
-export function defaultEqualsFalsy<T>(val1: T, val2: T, considerDistintFalsyValues = false): boolean | null {
-
-    if (!val1 || !val2)
-        return defaultEquals(val1, val2, considerDistintFalsyValues);
+export function defaultEqualsFalsy<T>(
+    val1: T,
+    val2: T,
+    considerDistintFalsyValues = false
+): boolean | null {
+    if (!val1 || !val2) return defaultEquals(val1, val2, considerDistintFalsyValues);
 
     return null;
+}
+
+export function encodeStringToBase64(rawStr: string): string {
+    if (isBlank(rawStr)) {
+        logDebug(`Failed to encode string to base64. 'rawStr' is ${rawStr}`);
+        return null;
+    }
+
+    return Buffer.from(rawStr).toString("base64");
+}
+
+export function encodeBase64(base64: string): string {
+    if (isBlank(base64)) {
+        logDebug(`Failed to encode base64. 'base64' is ${base64}`);
+        return null;
+    }
+
+    return Buffer.from(base64, "base64").toString();
+}
+
+/**
+ * base64Str -> json string -> obj.
+ *
+ * @param base64Str
+ * @returns
+ */
+export function encodeBase64ToObj(base64Str: string): any {
+    if (isBlank(base64Str)) {
+        logDebug(`Failed to encode base64 to obj. 'base64Str' is ${base64Str}`);
+        return null;
+    }
+
+    return JSON.parse(encodeBase64(base64Str));
+}
+
+/**
+ * obj -> json string -> base64.
+ * @param obj
+ * @returns
+ */
+export function encodeObjToBase64(obj: object): string | null {
+    if (isAnyFalsy(obj)) {
+        logDebug(`Failed to encode json to base64. 'obj' is ${obj}`);
+        return null;
+    }
+
+    return encodeStringToBase64(JSON.stringify(obj));
 }
