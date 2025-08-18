d8b4683f2a4a8342972dd8d122078318b56522b4
flobbe9 <florin735@live.com>
2025-08-18 20:55:05 +0200

    refactor: snackbar and toast sevirity styles

---
+++ abstract/NotificationSevirity.ts
@@ -0,0 +1,6 @@
+/**
+ * Use for popups like snackbar, toast etc
+ * 
+ * @since ?
+ */
+export type NotificationSevirity = "error" | "warn" | "info" | "success";
--- components/Toast.tsx
+++ components/Toast.tsx
@@ -11,52 +11,56 @@ import { useResponsiveStyles } from "@/hooks/useResponsiveStyles";
 import HelperReactChildren from "./helpers/HelperReactChildren";
 import { logDebug } from "@/utils/logUtils";
 import { GlobalContext } from "./context/GlobalContextProvider";
+import { NotificationSevirity } from "@/abstract/NotificationSevirity";
+import { NOTIFICATION_SEVIRITY_STYLES } from "@/utils/styleConstants";
 
 export interface GlobalToastProps {
     /** Applied to outer container */
-    outerStyle?: ViewStyle
+    outerStyle?: ViewStyle;
     /* Applied to scrollview wrapping the `content`. Remember to specify height and width explicitly for scroll to work */
-    childrenContainerStyle?: ViewStyle,
-    /** 
-     * Render a default footer with cancel and confirm buttons. To use a custom footer, set this to `false` 
-     * and pass children. Default is `true` 
+    childrenContainerStyle?: ViewStyle;
+    /**
+     * Render a default footer with cancel and confirm buttons. To use a custom footer, set this to `false`
+     * and pass children. Default is `true`
      */
-    defaultFooter?: boolean,
-    /** 
-     * Indicates to hide toast on dismiss. Includes footer buttons, touch outside and android's back button. 
+    defaultFooter?: boolean;
+    /**
+     * Indicates to hide toast on dismiss. Includes footer buttons, touch outside and android's back button.
      * Notice that setting this to `false` may result in an unescapable toast. In that case, overridden `onCancel` or
      * `onConfirm` should hide the toast.
      * Default is `true`
      */
-    hideOnDismiss?: boolean,
+    hideOnDismiss?: boolean;
+    /** Determines the toast style. Default is 'info' */
+    sevirity?: NotificationSevirity;
     /** See {@link ToastDefaultFooter}. Only works for `defaultFooter`. Hides toast, defined or not */
-    onConfirm?: () => void,
+    onConfirm?: () => void;
     /** See {@link ToastDefaultFooter}. Only works for `defaultFooter`. Hides toast, defined or not */
-    onCancel?: () => void
+    onCancel?: () => void;
     /** Called when toast closes, no matter how. */
-    onDismiss?: () => void,
-    visible: boolean,
-    content: ReactNode,
+    onDismiss?: () => void;
+    visible: boolean;
+    content: ReactNode;
     /** For defining a custom footer */
-    children?: ReactNode
+    children?: ReactNode;
 }
 
 interface Props extends HelperProps<ViewStyle>, ViewProps, GlobalToastProps {
-    hideToast: () => void
+    hideToast: () => void;
 }
-
 
 /**
  * Centered popup message overlaying the whole screen.
- * 
+ *
  * @since 0.0.1
  */
 export default function Toast({
     outerStyle = {},
-    childrenContainerStyle = {}, 
-    visible, 
+    childrenContainerStyle = {},
+    visible,
     hideOnDismiss = true,
     defaultFooter = true,
+    sevirity = "info",
     content,
     hideToast,
     onCancel,
@@ -69,54 +73,49 @@ export default function Toast({
     const componentName = "Toast";
     const { children, style, ...otherProps } = useHelperProps(props, componentName, ToastStyles.component);
 
-    function handleDimsiss(): void { 
-        if (onDismiss)
-            onDismiss();
+    function handleDimsiss(): void {
+        if (onDismiss) onDismiss();
 
-        if (hideOnDismiss)
-            hideToast();
+        if (hideOnDismiss) hideToast();
     }
 
     function handleCancel(): void {
-        if (onCancel)
-            onCancel();
+        if (onCancel) onCancel();
 
         handleDimsiss();
     }
 
     function handleConfirm(): void {
-        if (onConfirm)
-            onConfirm();
+        if (onConfirm) onConfirm();
 
         handleDimsiss();
     }
-    
+
     return (
         <Portal>
-            <Modal 
-                visible={visible} 
+            <Modal
+                visible={visible}
                 contentContainerStyle={{
-                    ...style as object,
-                    ...outerStyle as object,
+                    ...(style as object),
+                    ...NOTIFICATION_SEVIRITY_STYLES[sevirity],
+                    ...(outerStyle as object),
                 }}
-                onDismiss={handleDimsiss} 
+                onDismiss={handleDimsiss}
                 {...otherProps}
-            > 
+            >
                 <HelperScrollView dynamicStyle={ToastStyles.childrenContainer} style={childrenContainerStyle}>
                     <HelperReactChildren>{content}</HelperReactChildren>
-                </HelperScrollView> 
+                </HelperScrollView>
 
-                <HelperReactChildren>
-                    {children}
-                </HelperReactChildren>
+                <HelperReactChildren>{children}</HelperReactChildren>
 
-                <ToastDefaultFooter 
-                    style={{...prs("mt_2")}}
+                <ToastDefaultFooter
+                    style={{ ...prs("mt_2") }}
                     rendered={defaultFooter}
                     onCancel={handleCancel}
                     onConfirm={handleConfirm}
                 />
             </Modal>
         </Portal>
-    )
+    );
 }
