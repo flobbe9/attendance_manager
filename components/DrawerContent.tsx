d848dac20012994e357b15f604c94db5a6adbc08
flobbe9 <florin735@live.com>
2025-08-31 20:32:21 +0200

    fix: minor

--- components/DrawerContent.tsx
+++ components/DrawerContent.tsx
@@ -1,20 +1,15 @@
+import { FontAweSomeIconname } from "@/abstract/FontAwesomeIconName";
 import HelperProps from "@/abstract/HelperProps";
-import { DefaultComponentStyles } from "@/assets/styles/DefaultComponentStyles";
 import { DrawerContentStyles } from "@/assets/styles/DrawerContentStyles";
-import { LayoutStyles } from "@/assets/styles/LayoutStyles";
 import HelperView from "@/components/helpers/HelperView";
 import { useHelperProps } from "@/hooks/useHelperProps";
+import { LIGHT_COLOR } from "@/utils/styleConstants";
 import { FontAwesome } from "@expo/vector-icons";
-import { ExternalPathString, Href, Link, RelativePathString, useNavigation, useRouter } from "expo-router";
-import React, { JSX, ReactNode } from "react";
+import { Href, useRouter } from "expo-router";
+import React from "react";
 import { ViewProps, ViewStyle } from "react-native";
-import HelperText from "./helpers/HelperText";
-import Flex from "./helpers/Flex";
-import { FontAweSomeIconname } from "@/abstract/FontAwesomeIconName";
 import HelperButton from "./helpers/HelperButton";
-import { GLOBAL_SCREEN_PADDING, LIGHT_COLOR } from "@/utils/styleConstants";
-import HelperStyles from "@/assets/styles/helperStyles";
-import { HelperButtonStyles } from "@/assets/styles/HelperButtonStyles";
+import HelperText from "./helpers/HelperText";
 
 interface Props extends HelperProps<ViewStyle>, ViewProps {}
 
