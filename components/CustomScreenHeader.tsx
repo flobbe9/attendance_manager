4c82679245bc4e1e1bfa9a6616eabe679a6b50f9
flobbe9 <florin735@live.com>
2025-09-07 11:16:33 +0200

    minor

--- components/CustomScreenHeader.tsx
+++ components/CustomScreenHeader.tsx
@@ -1,12 +1,12 @@
 import HelperProps from "@/abstract/HelperProps";
+import { CustomScreenHeaderStyles } from "@/assets/styles/CustomScreenHeaderStyles";
 import HelperView from "@/components/helpers/HelperView";
 import { useHelperProps } from "@/hooks/useHelperProps";
-import { useResponsiveStyles } from "@/hooks/useResponsiveStyles";
 import React, { ReactNode, useContext } from "react";
 import { ViewProps, ViewStyle } from "react-native";
+import { GlobalContext } from "./context/GlobalContextProvider";
 import Flex from "./helpers/Flex";
 import HelperReactChildren from "./helpers/HelperReactChildren";
-import { GlobalContext } from "./context/GlobalContextProvider";
 
 interface Props extends HelperProps<ViewStyle>, ViewProps {
     leftContent?: ReactNode;
@@ -23,14 +23,14 @@ export default function CustomScreenHeader({ leftContent, centerContent, rightCo
     const { prs } = useContext(GlobalContext);
 
     const componentName = "CustomScreenHeader";
-    const { children, style, ...otherProps } = useHelperProps(props, componentName);
+    const { children, style, ...otherProps } = useHelperProps(props, componentName, CustomScreenHeaderStyles.component);
 
     return (
         <Flex
             justifyContent="space-between"
             alignItems="center"
             flexWrap="nowrap"
-            style={{ backgroundColor: "white", ...(style as object) }}
+            style={{ ...(style as object) }}
             {...otherProps}
         >
             <HelperView style={{ ...prs("col_2") }}>
